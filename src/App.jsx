67797b65194c10a23c7ad682d535d2f74daaf832
Janaka <janakactsl@gmail.com>
2026-01-27 21:42:49 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
 import styled from 'styled-components'
 import profileImage from './assets/me.jpg'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './stylesheet.css'
 
 const MainContainer = styled.table`
   width: 100%;
@@ -41,6 +37,8 @@ const BioCell = styled.td`
 
 const NameText = styled.p`
   text-align: center;
+  font-size: 32px;
+  font-weight: 500;
 `;
 
 const LinksText = styled.p`
@@ -60,77 +58,165 @@ const ProfileImage = styled.img`
   border-radius: 50%;
 `;
 
+const SectionHeader = styled.h2`
+  padding: 16px;
+  width: 100%;
+  vertical-align: middle;
+`;
 
-function Paper() {
-  return <td style="padding:8px;width:80%;vertical-align:middle">
-    <a href="https://m-niemeyer.github.io/nexf/">
-      <span class="papertitle">NExF: Learning Neural Exposure Fields for View Synthesis</span>
-    </a>
-    <br>
-      <a href="https://m-niemeyer.github.io/">Michael Niemeyer</a>,
-      <a href="https://campar.in.tum.de/Main/FabianManhardt">Fabian Manhardt</a>,
-      <a href="http://www.lix.polytechnique.fr/Labo/Marie-Julie.RAKOTOSAONA/">Marie-Julie Rakotosaona</a>,
-      <a href="https://moechsle.github.io">Michael Oechsle</a>,
-      <a href="https://ait.ethz.ch/people/ctsalico">Christina Tsalicoglou</a>,
-      <a href="https://scholar.google.com/citations?user=ml3laqEAAAAJ&hl=ja">Keisuke Tateno</a>,
-      <strong>Jonathan T. Barron</strong>,
-      <a href="https://federicotombari.github.io/">Federico Tombari</a>
-      <br>
-        <em>NeurIPS</em>, 2025
-        <br>
-          <a href="https://m-niemeyer.github.io/nexf/">project page</a>
-          /
-          <a href="https://arxiv.org/abs/2510.08279">arXiv</a>
-          <p></p>
-          <p>
-            Learning a neural field that optimizes exposure for each 3D point enables high-quality 3D-consistent view synthesis despite extreme exposure variation during capture.
-          </p>
-        </td>
-      </tr>
+const PaperRow = styled.tr`
+  background-color: ${props => props.highlight ? '#ffffd0' : 'transparent'};
+`;
+
+const ImageCell = styled.td`
+  padding: 16px;
+  width: 20%;
+  vertical-align: middle;
+`;
+
+const ContentCell = styled.td`
+  padding: 8px;
+  width: 80%;
+  vertical-align: middle;
+`;
+
+const PaperTitle = styled.a`
+  font-weight: bold;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HoverOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: ${props => props.isHovered ? 1 : 0};
+  transition: opacity 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PaperImg = styled.img`
+  width: 100%;
+  display: block;
+`;
+
+const PaperVideo = styled.video`
+  width: 100%;
+  height: auto;
+  display: block;
+`;
+
+const Venue = styled.em`
+
+`
+
+function Paper({ data }) {
+  return (
+    <PaperRow>
+      <ImageCell>
+        <ImageContainer>
+          <PaperImg src={data.image} />
+        </ImageContainer>
+      </ImageCell>
+      <ContentCell>
+        <PaperTitle href={data.projectPage || data.arxiv}>{data.title}</PaperTitle>
+        <br />
+        {data.authors}
+        <br />
+        <Venue>{data.venue}, </Venue>
+        {data.projectPage && <><a href={data.projectPage}>project page</a> / </>}
+        {data.arxiv && <a href={data.arxiv}>arXiv</a>}
+        {data.code && <a href={data.code}>code</a>}
+        {data.openreview && <a href={data.openreview}>OpenReview</a>}
+        <p>{data.description}</p>
+      </ContentCell>
+    </PaperRow>
+  );
 }
 
-      function App() {
-  const [count, setCount] = useState(0)
+const Person = styled.a`
 
-      return (
-      <>
-        <MainContainer>
-          <tbody>
-            <NoPaddingRow>
-              <NoPaddingCell>
-                <InnerContainer>
-                  <tbody>
-                    <NoPaddingRow>
-                      <BioCell>
-                        <NameText className="name">
-                          Janaka Brahmanage
-                        </NameText>
-                        <p>
-                          Bio:
-                        </p>
-                        <LinksText>
-                          <a href="mailto:janakactsl@gmail.com">Email</a> &nbsp;/&nbsp;
-                          <a href="data/Janaka-Brahmanage-cv.pdf">CV</a> &nbsp;/&nbsp;
-                          <a href="data/Janaka-Brahmanage-bio.txt">Bio</a> &nbsp;/&nbsp;
-                          <a href="https://scholar.google.com/citations?user=E7r7hTUAAAAJ&hl=en">Scholar</a> &nbsp;/&nbsp;
-                          <a href="https://twitter.com/janakact">Twitter</a> &nbsp;/&nbsp;
-                          <a href="https://github.com/janakact/">Github</a>
-                        </LinksText>
-                      </BioCell>
-                      <PhotoCell>
-                        <a href={profileImage}>
-                          <ProfileImage alt="profile photo" src={profileImage} className="hoverZoomLink" />
-                        </a>
-                      </PhotoCell>
-                    </NoPaddingRow>
-                  </tbody>
-                </InnerContainer>
-              </NoPaddingCell>
-            </NoPaddingRow>
-          </tbody>
-        </MainContainer>
-      </>
-      )
+`
+const Janaka = ({ link }) => link ? <Person href="https://janakact.github.io/">Janaka Chathuranga Brahmanage</Person> : <b>Janaka Chathuranga Brahmanage</b>
+const Akshat = () => <Person href="https://faculty.smu.edu.sg/profile/akshat-kumar-381">Akshat Kumar</Person>
+const Jiajing = () => <Person href="https://lingkaching.github.io/">LING Jiajing</Person>
+
+const papersData = [
+  {
+    title: "Flowpg: action-constrained policy gradient with normalizing flows",
+    authors: <><Janaka />, <Jiajing />, <Akshat /></>,
+    venue: "NeurIPS 2023",
+    projectPage: false,
+    arxiv: "https://arxiv.org/abs/2402.05149",
+    openreview: "https://openreview.net/forum?id=p1gzxzJ4Y5",
+    code: "https://github.com/rlr-smu/flow-pg",
+    image: "images/flowpg.jpg",
+    description: "Utilizes Normalizing Flows to map policy outputs directly to feasible action spaces, eliminating the need for slow optimization solvers.",
+  }
+];
+
+function App() {
+  return (
+    <MainContainer>
+      <tbody>
+        <NoPaddingRow>
+          <NoPaddingCell>
+            <InnerContainer>
+              <tbody>
+                <NoPaddingRow>
+                  <BioCell>
+                    <NameText className="name">Janaka Brahmanage</NameText>
+                    <p>Bio:</p>
+                    <LinksText>
+                      <a href="mailto:janakactsl@gmail.com">Email</a> &nbsp;/&nbsp;
+                      <a href="data/Janaka-Brahmanage-cv.pdf">CV</a> &nbsp;/&nbsp;
+                      <a href="data/Janaka-Brahmanage-bio.txt">Bio</a> &nbsp;/&nbsp;
+                      <a href="https://scholar.google.com/citations?user=E7r7hTUAAAAJ&hl=en">Scholar</a> &nbsp;/&nbsp;
+                      <a href="https://twitter.com/janakact">Twitter</a> &nbsp;/&nbsp;
+                      <a href="https://github.com/janakact/">Github</a>
+                    </LinksText>
+                  </BioCell>
+                  <PhotoCell>
+                    <a href={profileImage}>
+                      <ProfileImage alt="profile photo" src={profileImage} className="hoverZoomLink" />
+                    </a>
+                  </PhotoCell>
+                </NoPaddingRow>
+              </tbody>
+            </InnerContainer>
+
+            <InnerContainer>
+              <tbody>
+                <tr>
+                  <td>
+                    <SectionHeader>Research</SectionHeader>
+                  </td>
+                </tr>
+              </tbody>
+            </InnerContainer>
+
+            <InnerContainer>
+              <tbody>
+                {papersData.map((paper, index) => (
+                  <Paper key={index} data={paper} />
+                ))}
+              </tbody>
+            </InnerContainer>
+          </NoPaddingCell>
+        </NoPaddingRow>
+      </tbody>
+    </MainContainer>
+  );
 }
 
-      export default App
+export default App;
